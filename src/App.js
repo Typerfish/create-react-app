6c6cbbaedc2ae99143fc8a155ee9ae8a1d71040d
ireland <deannaireland@icloud.com>
2022-06-04 12:37:01 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import React
+import React, { useState } from 'react'
+
+// counter function
+function App(){
+    let [ counter, changeCounter ] = useState(1)
+    window.changeCounter = changeCounter
+    return (
+        <div>
+            <h1>{counter}</h1>
+            <h4>{counter}</h4>
+        </div>
+    )
+}
+
+export default App;
+
